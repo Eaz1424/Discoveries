5d6abb154950776d7794aed5dc6e1a722f77333d
Eaz1424 <111675362+Eaz1424@users.noreply.github.com>
2022-09-28 22:28:09 +0530

    Add files via upload

---
+++ rollNo.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+#include <cmath>
+#define pi 3.14
+
+
+using namespace std;
+
+
+int main()
+{
+    float roll;
+    cout<<"Enter your roll number";
+    cin>>roll;
+    
+    if((int)roll==roll)
+    {
+        cout<<"Valid Roll No"<<endl;
+
+    }
+else
+{
+   
+        cout<<"Invalid Roll No.";
+    
+}
+
+
+
+
+
+return 0;
+}  
