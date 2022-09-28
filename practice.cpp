71a9864abe7beb18bef208bf63213eac2907f40e
Eaz1424 <111675362+Eaz1424@users.noreply.github.com>
2022-09-28 16:34:08 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <cmath>
+#define pi 3.14
+typedef int marks; 
+
+using namespace std;
+
+
+int main()
+{
+    marks m1,m2,m3;
+    int avg;
+    cout<<"Enter the marks"<<endl;
+    cin>>m1>>m2>>m3;
+    avg=(m1+m2+m3)/3;
+    if(avg>=60)
+    {
+        cout<<"Congratulations,you have scored A"<<endl;
+
+    }
+else
+{
+    if(=<avg<60)
+}
+
+
+
+
+
+return 0;
+}  
